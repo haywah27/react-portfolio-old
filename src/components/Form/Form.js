102b81451ed5adc831d5f467687ddb0b6f357066
haywah27 <hayleywahlroos15@gmail.com>
2021-04-28 21:41:29 -0700

    contact page outline setup

---
+++ src/components/Form/Form.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Jumbotron, Container, Button, Form } from "react-bootstrap";
+import "./form.css";
+
+function ContactForm() {
+    function handleSumbit() {}
+
+    function cancelForm() {}
+
+  return (
+    <>
+      <Jumbotron>
+        <h2>Contact Form</h2>
+        <hr />
+        <Form>
+          <Form.Group controlId="formSubject">
+            <Form.Label>Subject</Form.Label>
+            <Form.Control type="text" placeholder="Anything specific?" />
+          </Form.Group>
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Ex: name@website.com" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formMessage">
+            <Form.Label>Message</Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="What's up?" />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="submitBtn" onClick={handleSumbit()}>
+            Submit
+          </Button>
+          <Button variant="primary" type="submit" className="cancelBtn" onClick={cancelForm()}>
+            Cancel
+          </Button>
+        </Form>
+      </Jumbotron>
+    </>
+  );
+}
+
+export default ContactForm;
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,14 +1,18 @@
 "i have an art and design background but decided to use those skills toward a technical applicaiton";
 
 import React from "react";
-// import "./contact.css";
-import { Jumbotron, Container, Button } from "react-bootstrap";
+import "./contact.css";
+import { Jumbotron, Container, Button, Form } from "react-bootstrap";
 import Navigation from "../components/Nav/Nav";
+import ContactForm from "../components/Form/Form";
 
 function Contact() {
   return (
     <div>
       <Navigation />
+      <Container className="formContainer">
+        <ContactForm />
+      </Container>
     </div>
   );
 }
